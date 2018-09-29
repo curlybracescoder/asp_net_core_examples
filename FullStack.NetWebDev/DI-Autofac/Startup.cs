87d61d8a0824f30ce903d629b050a0ccef68207d
Karthik K (CurlyBracesCoder) <curlybracescoder@gmail.com>
2018-09-29 21:47:13 +0530

    DI with Autofac

---
+++ FullStack.NetWebDev/DI-Autofac/Startup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using Autofac;
+
+using DI.Repositories;
+using Autofac.Extensions.DependencyInjection;
+
+namespace DI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public IContainer Container{get; private set;}
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            
+            var builder = new Autofac.ContainerBuilder();
+            builder.RegisterType<UserRepository>().As<IUserRepository>();
+            builder.Populate(services);
+
+            Container = builder.Build();
+            return new AutofacServiceProvider(Container);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime appLifeTime)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            appLifeTime.ApplicationStopped.Register(()=> Container.Dispose());
+        }
+    }
+}
